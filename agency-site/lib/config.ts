bb2901f4c793b7ff87f469167699e59132c62546
SaintSammie <akpansamue1688@gmail.com>
2026-07-24 20:24:36 +0200

    intial commit

--- agency-site/lib/config.ts
+++ agency-site/lib/config.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   shortName: "Monark",
   email: "hello@themonarkwebservices.com",
   phone: "+1 (000) 000-0000",
-  whatsapp: "10000000000",
+  whatsapp: "2349034552001",
   location: "Available worldwide",
   socials: {
     linkedin: "#",
